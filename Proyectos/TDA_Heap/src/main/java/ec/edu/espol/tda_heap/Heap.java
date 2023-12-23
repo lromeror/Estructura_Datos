6d36c13bf16fb2da159258170646a9bb20669074
Luis R. <lromeror@fiec.espol.edu.ec>
2023-12-23 01:29:02 -0500

    Canbios de Heat

--- Proyectos/TDA_Heap/src/main/java/ec/edu/espol/tda_heap/Heap.java
+++ Proyectos/TDA_Heap/src/main/java/ec/edu/espol/tda_heap/Heap.java
@@ -5,22 +5,116 @@
 package ec.edu.espol.tda_heap;
 
 import java.util.Comparator;
+import java.util.Iterator;
 
 /**
  *
- * @author Jonanyu 11.1
+ * @author Luis Romero
+ * @param <E>
  */
-public class Heap<E> {//Es una arreglo con mascara de binario
-    //Es un arbol con propiedad de orden por eso es necesario comparar
+public class Heap<E> {
 
     private E[] elements;
     private int MAX_SIZE = 100;
     private int effectiveSize;
-    private Comparator<E> cmp;//esto define si es ascendente o descendete
+    private final Comparator<E> cmp;
 
-    public Heap() {
+    public Heap(Comparator<E> cmp) {
         this.elements = (E[]) new Object[MAX_SIZE];
         this.effectiveSize = 0;
+        this.cmp = cmp;
     }
 
+    public int getLeft(int posRoot) {
+        int posLeft = 2 * posRoot + 1;
+        if (posRoot > effectiveSize - 1 || posRoot < 0 || posLeft > this.effectiveSize) {
+            return -1;
+        }
+        return posLeft;
+    }
+
+    public int getRight(int posRoot) {
+        int posRight = 2 * posRoot + 2;
+        if (posRoot > effectiveSize - 1 || posRoot < 0 || posRight > this.effectiveSize) {
+            return -1;
+        }
+        return posRight;
+    }
+
+    public int getParent(int posRoot) {
+        if (posRoot == 0 || posRoot > effectiveSize - 1 || posRoot < 0) {
+            return -1;
+        }
+        return (posRoot - 1) / 2;
+    }
+
+    public boolean isLeaf(int posRoot) {
+        return this.getLeft(posRoot) < 0 && this.getRight(posRoot) < 0;
+    }
+
+    private boolean isFull() {
+        return effectiveSize == MAX_SIZE;
+    }
+
+    private void addCapacity() {
+        MAX_SIZE = MAX_SIZE * 2;
+        E[] newElements = (E[]) new Object[MAX_SIZE];
+        for (int i = 0; i < effectiveSize; i++) {
+            newElements[i] = elements[i];
+        }
+        this.elements = newElements;
+    }
+
+    public boolean addFirst(E element) {
+        if (element == null) {
+            return false;
+        }
+        if (isFull()) {
+            addCapacity();
+        }
+        for (int i = this.effectiveSize - 1; i >= 0; i--) {
+            elements[i + 1] = elements[i];
+        }
+
+        this.elements[0] = element;
+        this.effectiveSize++;
+        return true;
+    }
+
+    public void ajustar(int posRoot) {
+        if (posRoot >= 0 && posRoot < effectiveSize - 1 && !this.isLeaf(posRoot)) {
+            E Left = this.elements[this.getLeft(posRoot)];
+            E Right = this.elements[this.getRight(posRoot)];
+            E Root = this.elements[posRoot];
+            if (cmp.compare(Root, Left) < 0) {
+                swap(this.getLeft(posRoot), Root, Left);
+                this.ajustar(this.getLeft(posRoot));
+            }
+            if (cmp.compare(Root, Right) < 0) {
+                swap(this.getRight(posRoot), Root, Right);
+                this.ajustar(this.getRight(posRoot));
+            }
+        }
+    }
+
+    public void swap(int posRoot, E eleRoot, E mayorEle) {
+        this.elements[posRoot] = eleRoot;
+        this.elements[this.getParent(posRoot)] = mayorEle;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder cadena = new StringBuilder();
+        cadena.append("[");
+        for (int i = 0; i <= this.effectiveSize - 1; i++) {
+            if (i == this.effectiveSize - 1) {
+                cadena.append(this.elements[i]);
+            } else {
+                cadena.append(this.elements[i]);
+                cadena.append(",");
+            }
+        }
+        cadena.append("]");
+        return cadena.toString();
+    }
 }
--- Proyectos/TDA_Heap/src/main/java/ec/edu/espol/tda_heap/TDA_Heap.java
+++ Proyectos/TDA_Heap/src/main/java/ec/edu/espol/tda_heap/TDA_Heap.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.espol.tda_heap;
+
+import java.util.Comparator;
 
 /**
  *
@@ -11,6 +12,25 @@ package ec.edu.espol.tda_heap;
 public class TDA_Heap {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Comparator<Integer> cmp = (Integer o1, Integer o2) -> o1 - o2;
+
+        Heap<Integer> nums = new Heap<>(cmp);
+        nums.addFirst(28);
+        nums.addFirst(70);
+        nums.addFirst(20);
+        nums.addFirst(72);
+        nums.addFirst(25);
+        nums.addFirst(30);
+        nums.addFirst(80);
+        nums.addFirst(75);
+        nums.addFirst(60);
+        nums.addFirst(150);
+        
+        System.out.println(nums);
+        System.out.println(nums.getLeft(5));
+        System.out.println(nums.isLeaf(5));
+        System.out.println(nums.getParent(3));
+        nums.ajustar(1);
+        System.out.println(nums);
     }
 }
