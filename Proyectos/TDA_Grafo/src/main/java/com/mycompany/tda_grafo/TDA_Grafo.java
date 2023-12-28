4cfd515fdfe1bb150d7d5de002857599851c45f9
Luis R. <lromeror@fiec.espol.edu.ec>
2023-12-28 02:25:36 -0500

    Solo creacion del proyecto TDA Grafo

---
+++ Proyectos/TDA_Grafo/src/main/java/com/mycompany/tda_grafo/TDA_Grafo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tda_grafo;
+
+/**
+ *
+ * @author Luis Romero
+ */
+public class TDA_Grafo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
